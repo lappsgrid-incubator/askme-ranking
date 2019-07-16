6cbf81741843497b461d21f91d5d84e6800b5aa2
Kevin Ros <kevinros1190@gmail.com>
2019-07-15 20:07:31 -0400

    neg. changes

--- src/main/groovy/org/lappsgrid/eager/mining/ranking/RankingProcessor.groovy
+++ src/main/groovy/org/lappsgrid/eager/mining/ranking/RankingProcessor.groovy
@@ -4,19 +4,25 @@ import groovy.util.logging.Slf4j
 import org.lappsgrid.eager.mining.api.Query
 import org.lappsgrid.eager.mining.ranking.model.Document
 
+import org.lappsgrid.rabbitmq.Message
+import org.lappsgrid.rabbitmq.topic.MessageBox
+import org.lappsgrid.rabbitmq.topic.PostOffice
+
 import java.util.concurrent.ExecutorCompletionService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
 @Slf4j('logger')
-class RankingProcessor {
+class RankingProcessor{
 
     ExecutorCompletionService<Document> executor
     CompositeRankingEngine engines
 
+
     RankingProcessor(Map params) {
         this(Runtime.getRuntime().availableProcessors(), params)
      }
+
 
     RankingProcessor(int nThreads, Map params) {
         executor = new ExecutorCompletionService<>(Executors.newFixedThreadPool(nThreads))
