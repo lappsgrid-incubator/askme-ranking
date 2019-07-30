a3300b811d578c7dee387548047445a63ef09184
Kevin Ros <kevinros1190@gmail.com>
2019-07-30 10:34:45 -0400

    removes old ranking processor when message is received from askme-web

--- src/main/groovy/org/lappsgrid/eager/mining/ranking/Main.groovy
+++ src/main/groovy/org/lappsgrid/eager/mining/ranking/Main.groovy
@@ -1,7 +1,6 @@
 package org.lappsgrid.eager.mining.ranking
 
 import groovy.util.logging.Slf4j
-import org.apache.calcite.adapter.java.Map
 import org.apache.solr.common.SolrDocument
 import org.apache.solr.common.SolrDocumentList
 import org.lappsgrid.eager.mining.api.Query
@@ -23,6 +22,7 @@ class Main extends MessageBox{
     static final String EXCHANGE = "org.lappsgrid.query"
     PostOffice po = new PostOffice(EXCHANGE, HOST)
     Stanford nlp = new Stanford()
+    Map ranking_processors = [:]
 
 
     Main(){
@@ -30,36 +30,48 @@ class Main extends MessageBox{
 
     }
 
+    //TO-DO
+    //2. dealing with lists?
+    //3. add new method to rank
+
     void recv(Message message){
         String id = message.getId()
-        String num = message.getCommand()
-        logger.info("Received document {} from query {}", num, id)
-        Object params = message.getParameters()
-
-        Object dq = message.body
-        Query q = dq.query
-        SolrDocument solr = dq.document
-        Document doc = new Document()
-        ['id', 'pmid','pmc','doi','year','path'].each { field ->
-            doc.setProperty(field, solr.getFieldValue(field))
+        String command = message.getCommand()
+        if(command == "remove_ranking_processor"){
+            ranking_processors.remove(id)
+            logger.info('Removed ranking processor {}',id)
         }
+        else {
+            logger.info("Received document {} from query {}", command, id)
+            Object params = message.getParameters()
+            Object dq = message.body
+            Query q = dq.query
+            SolrDocument solr = dq.document
+            Document doc = new Document()
+            ['id', 'pmid', 'pmc', 'doi', 'year', 'path'].each { field ->
+                doc.setProperty(field, solr.getFieldValue(field))
+            }
 
-        Section title = nlp.process(solr.getFieldValue('title').toString())
-        doc.setProperty('title', title)
+            Section title = nlp.process(solr.getFieldValue('title').toString())
+            doc.setProperty('title', title)
 
-        Section abs = nlp.process(solr.getFieldValue('abstract').toString())
-        doc.setProperty('articleAbstract', abs)
+            Section abs = nlp.process(solr.getFieldValue('abstract').toString())
+            doc.setProperty('articleAbstract', abs)
 
-        List document = [doc]
+            List document = [doc]
 
-        RankingProcessor ranker = new RankingProcessor(params)
+            if (!ranking_processors.containsKey(id)) {
+                ranking_processors."${id}" = new RankingProcessor(params)
+            }
+            RankingProcessor ranker = ranking_processors."${id}"
 
-        List<Document> sorted = ranker.rank(q, document)
-        logger.info('Score:{}', sorted[0].getScore())
-        logger.info('Sending ranked document {} from message {} back to web',num,id)
-        message.setBody(sorted[0])
-        message.setRoute([WEB_MBOX])
-        po.send(message)
+            List<Document> sorted = ranker.rank(q, document)
+            logger.info('Score:{}', sorted[0].getScore())
+            logger.info('Sending ranked document {} from message {} back to web', command, id)
+            message.setBody(sorted[0])
+            message.setRoute([WEB_MBOX])
+            po.send(message)
+        }
 
     }
 
